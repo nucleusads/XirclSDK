c4daff2cd5ff4f772e75a20378c7038042890c6c
syntaxerror1972 <syntaxerror1972>
2017-07-21 18:26:03 +0530

    Copyrights © 2017 All Rights Reserved. FirstNucleus Internet Technologies Pvt. Ltd.

---
+++ xirclplugin/build/generated/source/buildConfig/release/firstnucleus/xirclplugin/lib/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package firstnucleus.xirclplugin.lib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "firstnucleus.xirclplugin.lib";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
